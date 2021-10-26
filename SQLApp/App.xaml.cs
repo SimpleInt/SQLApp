c7f72722cdb8601db73e4e2f0ae1b1ca6c8671d7
SimpleInt <92132071+SimpleInt@users.noreply.github.com>
2021-10-25 19:19:04 -0500

    Add files via upload

---
+++ SQLApp/App.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using Azure.Storage.Blobs;
+using SQLApp.resources.lang;
+using SQLApp.resources.post;
+
+namespace SQLApp
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    IE11WebBrowserRegisitry.ChangeInternetExplorerVersion();
+        //}
+
+        //protected override void OnExit(ExitEventArgs e)
+        //{
+        //    base.OnExit(e);
+        //    IE11WebBrowserRegisitry.RemoveRegistry();
+        //}
+        private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			checkIcons();
+			MainWindow mw = new MainWindow(ReadSetting("lang"));
+			mw.Show();
+		}
+		#region This only download the images (example: edit.png)
+		static void checkIcons()
+        {
+            List<string> files = new List<string>();
+            files.Add("edit.png");
+            files.Add("delete.png");
+            files.Add("download.png");
+            var filters = new String[] { "png" };
+
+            try
+            {
+                string[] s = GetFilesFrom(Directory.GetCurrentDirectory() + @"\icons", filters, false);
+                for (int i = 0; i < files.Count; i++)
+                {
+                    if (files.Contains(s[i]) == false)
+                    {
+                        Download($"{files[i]}", Directory.GetCurrentDirectory() + $@"\icons\{files[i]}");
+                    }
+                }
+            }
+            catch
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\icons");
+                for (int i = 0; i < files.Count; i++)
+                {
+                    Download($"{files[i]}", Directory.GetCurrentDirectory() + $@"\icons\{files[i]}");
+                }
+            }
+        }
+        public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+        static void Download(string filename, string path)
+        {
+            BlobClient blobClient = new BlobClient(
+                connectionString: sql.ConnectionString,
+                blobContainerName: "icons",
+                blobName: $"{filename}");
+
+
+            blobClient.DownloadTo(path);
+        }
+        #endregion
+        #region ReadSettings: lang, email & password
+        public static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Error");
+                return "False";
+            }
+        }
+
+        public static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Error writing app settings");
+            }
+        }
+        #endregion
+    }
+}
---
+++ SQLApp/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using SQLApp.resources.net;
+using SQLApp.resources.lang;
+
+namespace SQLApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static string lang { get; set; }
+		#region Constructor
+		public MainWindow(string lang)
+		{
+			InitializeComponent();
+			txtEmail.Text = App.ReadSetting("email");
+			changeLang(lang);
+		}
+
+		#endregion
+		#region RegisterAndLogin
+		private void btnCheck_Click(object sender, RoutedEventArgs e)
+		{
+			check.Email(txtUserName.Text, txtEmail.Text, txtPassword.Password);
+		}
+		private void btnCheckLogin_Click(object sender, RoutedEventArgs e)
+		{
+			_ = Connetion.into(txtEmail.Text,Encrypt.encrypt(txtPassword.Password), this);
+
+		}
+		#endregion
+		#region 'Span'
+		static bool isRegister = true;
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			txtEmail.Clear();
+			txtPassword.Clear();
+			if (isRegister == true)
+			{
+				username.Visibility = Visibility.Visible;
+				txtUserName.Visibility = Visibility.Visible;
+				lblSpam.Visibility = Visibility.Visible;
+				lblSpamLogin.Visibility = Visibility.Hidden;
+
+				btnChecklogin.IsEnabled = false;
+				btnChecklogin.Visibility = Visibility.Hidden;
+				btnCheck.IsEnabled = true;
+				btnCheck.Visibility = Visibility.Visible;
+				lblTitleLogin.Visibility = Visibility.Hidden;
+				lblTitleSing.Visibility = Visibility.Visible;
+				isRegister = false;
+			}
+			else
+			{
+				username.Visibility = Visibility.Hidden;
+				txtUserName.Visibility = Visibility.Hidden;
+				lblSpam.Visibility = Visibility.Hidden;
+				lblSpamLogin.Visibility = Visibility.Visible;
+
+				btnChecklogin.IsEnabled = true;
+				btnChecklogin.Visibility = Visibility.Visible;
+				btnCheck.IsEnabled = false;
+				btnCheck.Visibility = Visibility.Hidden;
+
+
+				lblTitleLogin.Visibility = Visibility.Visible;
+				lblTitleSing.Visibility = Visibility.Hidden;
+				isRegister = true;
+			}
+		}
+		private void btnEnter(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{
+				if (btnChecklogin.IsEnabled == true)
+				{
+					
+					_ = Connetion.into(txtEmail.Text, Encrypt.encrypt(txtPassword.Password), this);
+
+				}
+				else
+				{
+					check.Email(txtUserName.Text, txtEmail.Text, txtPassword.Password);
+				}
+			}
+		}
+		#endregion
+		#region Change idiom
+		private void rdSp_Checked(object sender, RoutedEventArgs e)
+		{
+			changeLang("es-ES");
+			rdChi.IsChecked = false;
+			rdEn.IsChecked = false;
+			rdRus.IsChecked = false;
+		}
+		private void rdEn_Checked(object sender, RoutedEventArgs e)
+		{
+			changeLang("");
+			rdChi.IsChecked = false;
+			rdSp.IsChecked = false;
+			rdRus.IsChecked = false;
+		}
+
+		private void rdRus_Checked(object sender, RoutedEventArgs e)
+		{
+			changeLang("ru-Ru");
+			rdChi.IsChecked = false;
+			rdSp.IsChecked = false;
+			rdEn.IsChecked = false;
+		}
+
+		private void rdChi_Checked(object sender, RoutedEventArgs e)
+		{
+			changeLang("zh-CN");
+			rdEn.IsChecked = false;
+			rdSp.IsChecked = false;
+			rdRus.IsChecked = false;
+		}
+		void changeLang(string language)
+		{
+			App.AddUpdateAppSettings("lang", language);
+
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(App.ReadSetting("lang"));
+
+			lblTitleLogin.Content = idiom.TID_LOGIN;
+			lblTitleSing.Content = idiom.TID_REGISTER;
+
+			lblSpam.Text = idiom.TID_SpanRegister;
+			lblSpamLogin.Text = idiom.TID_SpanLogin;
+			btnClickme.Content = idiom.TID_SpanClic;
+
+			Title = idiom.TID_TITLE;
+			btnChecklogin.Content = idiom.TID_LOGIN;
+			btnCheck.Content = idiom.TID_REGISTER;
+
+			checkIdiom();
+		}
+		void checkIdiom()
+		{
+			switch (App.ReadSetting("lang"))
+			{
+				case "es-ES":
+					rdChi.IsChecked = false;
+					rdEn.IsChecked = false;
+					rdRus.IsChecked = false;
+					rdSp.IsChecked = true;
+					break;
+				case "ru-Ru":
+					rdRus.IsChecked = true;
+					rdChi.IsChecked = false;
+					rdEn.IsChecked = false;
+					rdSp.IsChecked = false;
+					break;
+				case "zh-CN":
+					rdChi.IsChecked = true;
+					rdEn.IsChecked = false;
+					rdRus.IsChecked = false;
+					rdSp.IsChecked = false;
+					break;
+				case "":
+					rdChi.IsChecked = false;
+					rdEn.IsChecked = true;
+					rdRus.IsChecked = false;
+					rdSp.IsChecked = false;
+					break;
+				default:
+					break;
+			}
+		}
+		#endregion
+	}
+}
